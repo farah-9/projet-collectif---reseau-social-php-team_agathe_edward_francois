286cb9c79f358db1803b831f294d4536d47635f7
Agathe Szkola <agathe.szkola@gmail.com>
2023-05-30 16:33:44 +0200

    Ajout tags et noms utilisatrices dans tag

--- Niveau1/admin.php
+++ Niveau1/admin.php
@@ -1,4 +1,4 @@
-0<!doctype html>
+<!doctype html>
 <html lang="fr">
     <head>
         <meta charset="utf-8">
@@ -61,13 +61,13 @@
                  */
                 while ($tag = $lesInformations->fetch_assoc())
                 {
-                    echo "<pre>" . print_r($tag, 1) . "</pre>";
+                    // echo "<pre>" . print_r($tag, 1) . "</pre>";
                     ?>
                     <article>
-                        <h3>#chaussette</h3>
-                        <p>id:321</p>
+                        <h3>#<?php echo($tag['label'])?></h3>
+                        <p>id:<?php echo($tag['id'])?></p>
                         <nav>
-                            <a href="tags.php?tag_id=321">Messages</a>
+                            <a href="tags.php?tag_id=$tag['id']">Messages</a>
                         </nav>
                     </article>
                 <?php } ?>
@@ -97,14 +97,14 @@
                     echo "<pre>" . print_r($tag, 1) . "</pre>";
                     ?>
                     <article>
-                        <h3>Alexandra</h3>
-                        <p>id:123</p>
+                        <h3><?php echo($tag['alias'])?></h3>
+                        <p>id:<?php echo($tag['id'])?></p>
                         <nav>
-                            <a href="wall.php?user_id=123">Mur</a>
-                            | <a href="feed.php?user_id=123">Flux</a>
-                            | <a href="settings.php?user_id=123">Paramètres</a>
-                            | <a href="followers.php?user_id=123">Suiveurs</a>
-                            | <a href="subscriptions.php?user_id=123">Abonnements</a>
+                            <a href="wall.php?user_id=<?php $tag['id']?>">Mur</a>
+                            | <a href="feed.php?user_id=<?php $tag['id']?>">Flux</a>
+                            | <a href="settings.php?user_id=<?php $tag['id']?>">Paramètres</a>
+                            | <a href="followers.php?user_id=<?php $tag['id']?>">Suiveurs</a>
+                            | <a href="subscriptions.php?user_id=<?php $tag['id']?>">Abonnements</a>
                         </nav>
                     </article>
                 <?php } ?>
