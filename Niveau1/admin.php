2a5f05701f209b819b99b939d356425f5453854e
Agathe Szkola <agathe.szkola@gmail.com>
2023-05-30 16:35:01 +0200

    commentaire de echo objet tag

--- Niveau1/admin.php
+++ Niveau1/admin.php
@@ -94,7 +94,7 @@
                  */
                 while ($tag = $lesInformations->fetch_assoc())
                 {
-                    echo "<pre>" . print_r($tag, 1) . "</pre>";
+                    // echo "<pre>" . print_r($tag, 1) . "</pre>";
                     ?>
                     <article>
                         <h3><?php echo($tag['alias'])?></h3>
